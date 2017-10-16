fca1e5281caebe231a76ab5c13134774d9318236
Greg Linklater <greglink49@gmail.com>
2017-10-16 13:24:17 +0200

    feat(Selector): selector registry type getter

--- src/Selector.js
+++ src/Selector.js
@@ -6,10 +6,33 @@
  */
 
 /**
- * Selector
+ * Module Dependencies
  * @ignore
  */
-class Selector {
+const Entry = require('./Entry')
+const Registry = require('./Registry')
+
+/**
+ * Selector
+ *
+ * @class Selector
+ * @classdesc
+ * An abstract Selector query.
+ *
+ * @hideconstructor
+ *
+ * @extends {Entry}
+ */
+class Selector extends Entry {
+
+  /**
+   * @static
+   * @constant
+   * @return {RegistryType}
+   */
+  static get registryType () {
+    return Registry.SELECTOR_REGISTRY_TYPE
+  }
 }
 
 /**
