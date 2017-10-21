be5097f20045a88956ec684e6e84f6d956812679
Greg Linklater <greglink49@gmail.com>
2017-10-21 18:15:12 +0200

    example(debugSyncRemote): replicate state as well

--- examples/debugSyncRemote.js
+++ examples/debugSyncRemote.js
@@ -10,3 +10,4 @@ const PouchDB = require('pouchdb')
  * Replicate to Remote Instance
  */
 new PouchDB('data/events_user').replicate.to('http://localhost:5984/events')
+new PouchDB('data/state_user').replicate.to('http://localhost:5984/state')
