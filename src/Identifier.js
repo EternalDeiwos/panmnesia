ab6da6b5f8f32a3821e658dc93154ed0e691a8ee
Greg Linklater <greglink49@gmail.com>
2017-10-21 20:36:44 +0200

    doc(Identifier): define and document identifier generator before export

--- src/Identifier.js
+++ src/Identifier.js
@@ -7,10 +7,21 @@
 const uuid = require('uuid/v1')
 
 /**
- * Exports
- * @ignore
+ * createIndexableIdentifier
+ * @private
+ *
+ * @description
+ * Mutate a freshly generated UUIDv1 into a time-based indexable identitifer string.
+ *
+ * @return {String}
  */
-module.exports = () => {
+const createIndexableIdentifier = () => {
   const id = uuid().split('-')
   return [id[2], id[1], id[0], id[3], id[4]].join('')
 }
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports = createIndexableIdentifier
